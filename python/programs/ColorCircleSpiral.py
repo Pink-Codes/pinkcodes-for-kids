a43ce0132d5e87098370970403f6196428d3a87a
aniqa-pinkcodes <127173735+aniqa-pinkcodes@users.noreply.github.com>
2023-03-08 19:55:53 -0800

    Add files via upload

---
+++ python/programs/ColorCircleSpiral.py
@@ -0,0 +1,10 @@
+# ColorCircleSpiral.py
+import turtle
+t= turtle.Pen()
+turtle.bgcolor("black")
+t.speed(0)
+colors = [ "red" , "blue" , "pink" , "green" ]
+for x in range(192):
+    t.pencolor( colors[ x % 4])
+    t.circle(x)
+    t.left(90)
