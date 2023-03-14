6cb76673057971514c4db57b7959c70f7704b9a4
aniqa-pinkcodes <127173735+aniqa-pinkcodes@users.noreply.github.com>
2023-03-13 20:48:46 -0700

    Add files via upload

---
+++ python/programs/ThankYou.py
@@ -0,0 +1,7 @@
+# ThankYou.py
+my_name = "Aniqa"
+your_name = input("What is your name?")
+print( "My name is " , my_name ) 
+print("Your name is " , your_name )
+print("Thank you for seeing this " , your_name ," !")
+
