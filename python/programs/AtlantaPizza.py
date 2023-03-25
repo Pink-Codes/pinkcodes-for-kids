e29a4c2a2961030bddd36d57b00e8b4213530198
aniqa-pinkcodes <127173735+aniqa-pinkcodes@users.noreply.github.com>
2023-03-25 12:13:23 -0700

    Delete AtlantaPizza.py

--- python/programs/AtlantaPizza.py
+++
@@ -1,8 +0,0 @@
-# AtlantaPizza.py
-number_of_pizzas = eval(input("How many pizzas do you want?"))
-cost_per_pizzas = eval(input("How much does each pizza cost?"))
-subtotal = number_of_pizzas  *  cost_per_pizzas
-tax_rate = 0.08
-sales_tax = subtotal * tax_rate
-total = subtotal + sales_tax
-print("The total cost is $" , total)
